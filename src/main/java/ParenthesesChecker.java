2819afe6d05a5d6813a110919d03ee6cde9e97f7
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-08-25 15:09:02 +0100

    implemented logic to sort the parentheses and compare equality of quantity

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -10,19 +10,22 @@ public class ParenthesesChecker {
         List<String> stringToList = new ArrayList<>(Arrays.asList(string.split("")));
 
         //create new lists to organise the parentheses and separate them from the rest of the string
-        List<String> openingParentheses = new ArrayList<>();
-        List<String> closingParentheses = new ArrayList<>();
+        List<String> openingBrackets = new ArrayList<>();
+        List<String> closingBrackets = new ArrayList<>();
 
-        //iterate through list in search of parentheses and add them to separate lists
+        //iterate through list in search of parentheses and add them to their own lists
         stringToList.forEach(character -> {
             if (character.equals("(") || character.equals("[") || character.equals("{") || character.equals("<")) {
-                openingParentheses.add(character);
+                openingBrackets.add(character);
             } else if (character.equals(")") || character.equals("]") || character.equals("}") || character.equals(">")) {
-                closingParentheses.add(character);
+                closingBrackets.add(character);
             }
         });
 
-        System.out.println(openingParentheses.size() + " " + closingParentheses.size());
+        //in case they have equal number of items, check if openings match closings
+        if (openingBrackets.size() == closingBrackets.size()) {
+            return true;
+        }
 
         return false;
     }
